a1b7ef67918d1a3c09e43c0aa84e4d457ef50da6
itHusarz <ithusarz@gmail.com>
2024-03-11 11:27:51 +0100

    header section

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+const navMobile = document.querySelector('.nav-mobile')
+const navBtn = document.querySelector('.hamburger')
+const footerYear = document.querySelector('.footer__year')
+
+const handleNav = () => {
+    navBtn.classList.toggle('is-active')
+    navMobile.classList.toggle('nav-mobile--active')
+}
+
+
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear()
+	footerYear.textContent = year
+}
+handleCurrentYear()
+
+
+navBtn.addEventListener('click', handleNav)
